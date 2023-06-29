2cb3eb0d934ebb9ff506b4fa0f98ad9b00d73106
rudikelelawar <bimogempar@gmail.com>
2023-06-30 02:09:42 +0700

    try data fetching

--- src/containers/Home/SecondSection/Categories/CategoryCard/index.tsx
+++ src/containers/Home/SecondSection/Categories/CategoryCard/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const CategoryCard = () => {
+const CategoryCard = ({item}) => {
     return (
         <article className="flex flex-col w-[182px]">
             <figure className="relative w-full h-[179px] rounded-xl overflow-hidden mb-5">
@@ -13,7 +13,7 @@ const CategoryCard = () => {
             </figure>
 
             <p className="text-heading-3 text-gray-100 font-bold">
-                Gunung
+                {item.category_icon}
             </p>
 
         </article>
--- src/containers/Home/SecondSection/Categories/index.tsx
+++ src/containers/Home/SecondSection/Categories/index.tsx
@@ -1,14 +1,28 @@
+import React, {useEffect, useState} from 'react';
 import CategoryCard from './CategoryCard';
 
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    const fetchData = async () => {
+        const req = await fetch('http://103.179.254.140:4444/categories')
+        console.log('this req', req)
+        const res = await req.json()
+        setCategories(res.data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
     return (
         <>
             <h3 className="text-heading-3 text-gray-100 font-label font-bold pt-14 mb-8">
                 Kategori Tempat
             </h3>
             <div className="flex justify-between">
-                {Array.from({ length: 6 }).map((_, index) => (
-                    <CategoryCard key={index} />
+                {categories.map((x, index) => (
+                    <CategoryCard key={index} item={x} />
                 ))}
             </div>
         </>
